6ff6808e2eb0ce97e108240239f03894e71a774a
Emmanuel Okp <okpunorrex@gmail.com>
2022-07-07 19:45:08 +0100

    added count

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { FiPlus } from "react-icons/fi";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleState } from "../../store/features/showForm/ShowFormSlice";
 import { resetSingleTodo } from "../../store/features/todo/todoSlice";
 
 export const Header = () => {
 	const dispatch = useDispatch();
+	const todosCount = useSelector((state) => state.todos.todos.length);
 
 	return (
 		<div className="header border text-capitalize d-flex justify-content-between ">
 			<div className="d-flex justify-content-between align-items-center text-secondary fs-6  d-flex flex--grow-1 px-3">
-				TASKS 0
+				TASKS {todosCount}
 			</div>
 			<div
 				className="d-flex justify-content-center align-items-center border-start add-button"
